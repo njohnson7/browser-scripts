5132196d78a62dd90b7529f8e5f57a1795165d55
Nick Johnson <njohnson7@pm.me>
2019-03-06 10:00:49 -0800

    clean up code

--- load-vim.js
+++ load-vim.js
@@ -2,9 +2,11 @@
 // Idea from: https://github.com/thomcom/autovim
 // Directions: save as a Chrome snippet, load a page that has CodeMirror editor (eg, Pramp), and run snippet.
 
+const CODEMIRROR_VERSION = '5.39.2'
+
 const importVimJs = () => {
-  const vimScriptElem = document.createElement('script')
-  vimScriptElem.src = 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.39.2/keymap/vim.js'
+  let vimScriptElem = document.createElement('script')
+  vimScriptElem.src = `https://cdnjs.cloudflare.com/ajax/libs/codemirror/${CODEMIRROR_VERSION}/keymap/vim.js`
   document.head.appendChild(vimScriptElem)
 }
 
@@ -35,37 +37,3 @@ CodeMirror.defineInitHook(loadVim)
 setTimeout(loadVim, 1000)
 
 console.log('Injected load-vim.js...')
-
-
-
-// #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-// #~~~~~~~~~~~~~~~~~ ORIGINAL ~~~~~~~~~~~~~~~~~#
-// #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-//let vimScript = document.createElement('script')
-//vimScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.39.2/keymap/vim.js'
-//document.head.appendChild(vimScript)
-
-//function loadVim() {
-//  var turn_on = function (e) {
-//    //console.log('Enable vimMode');
-//    // If I receive focus again...
-//    e.CodeMirror.on("focus", function(cm) {
-//      console.log(cm);
-//      e.CodeMirror.setOption("vimMode", true);
-//      e.CodeMirror.options.keyMap = 'vim';
-//      e.CodeMirror.options.showCursorWhenSelecting = 'vim';
-//    });
-//    // In case I already have focus...
-//    setTimeout(function(){
-//      e.CodeMirror.setOption("vimMode", true);
-//      e.CodeMirror.options.keyMap = 'vim';
-//      e.CodeMirror.options.showCursorWhenSelecting = 'vim';
-//    },200);
-//  }
-//  document.querySelectorAll(".CodeMirror").forEach(function(e){ turn_on(e); });
-//}
-
-//CodeMirror.defineInitHook(enable_vim);
-//console.log('injected load-vim.js...');
-//setTimeout(loadVim, 1000);
